808fecf5fc58dcf6f690d2526770cd6221ca9d7f
Petteri Särkkä <petteri.sarkka@fmail.com>
2021-01-14 10:06:59 +0200

    github link

--- projects/views.py
+++ projects/views.py
@@ -8,6 +8,6 @@ from django.http import HttpResponse
 def index(request):
     queryset = Project.objects.all() # list of objects
     context = {
-        "object_list": queryset
+        "object_list": queryset,
     }
     return render(request, "projects/index.html", context)
